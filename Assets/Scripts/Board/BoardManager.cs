02443baad8c0507d7b51c6e83092013d9437828c
raul-VJ25 <1881623@alu.murciaeduca.es>
2026-06-17 11:05:53 +0200

    enc

--- Assets/Scripts/Board/BoardManager.cs
+++ Assets/Scripts/Board/BoardManager.cs
@@ -1,60 +1,65 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization; // NECESARIO PARA RECUPERAR REFERENCIAS
 using UnityEngine.Tilemaps;
 
 // Gestor del tablero de juego
-// Crea y administra el mapa procedural con tiles, objetos y enemigos
 public class BoardManager : MonoBehaviour
 {
-    // Clase interna para datos de cada celda
     public class CellData
     {
-        public bool Passable;
-        public CellObject ContainedObject;
+        [SerializeField] private bool m_Passable;
+        [SerializeField] private CellObject m_ContainedObject;
+
+        public bool Passable { get => m_Passable; set => m_Passable = value; }
+        public CellObject ContainedObject { get => m_ContainedObject; set => m_ContainedObject = value; }
     }
 
-    // Referencias de componentes
-    public ExitCellObject ExitCellPrefab;
+    [Header("Prefabs de Objetos")]
+    [FormerlySerializedAs("ExitCellPrefab")][SerializeField] private ExitCellObject m_ExitCellPrefab;
+    [FormerlySerializedAs("FoodPrefabs")][SerializeField] private FoodObject[] m_FoodPrefabs;
+    [FormerlySerializedAs("GemPrefabs")][SerializeField] private XPGemObject[] m_GemPrefabs;
+    [FormerlySerializedAs("ChestPrefab")][SerializeField] private ChestObject m_ChestPrefab;
+    [FormerlySerializedAs("WallPrefab")][SerializeField] private WallObject m_WallPrefab;
+    [FormerlySerializedAs("EnemyPrefabs")][SerializeField] private EnemyObject[] m_EnemyPrefabs;
+
+    [Header("Dimensiones del Tablero")]
+    [FormerlySerializedAs("Width")][SerializeField] private int m_Width = 8;
+    [FormerlySerializedAs("Height")][SerializeField] private int m_Height = 8;
+
+    [Header("Tiles")]
+    [FormerlySerializedAs("GroundTiles")][SerializeField] private Tile[] m_GroundTiles;
+    [FormerlySerializedAs("WallTiles")][SerializeField] private Tile[] m_WallTiles;
+
+    [Header("Densidad Objetos")]
+    [FormerlySerializedAs("EnemyDensity")][Range(0, 15)][SerializeField] private int m_EnemyDensity = 2;
+    [FormerlySerializedAs("WallDensity")][Range(0, 15)][SerializeField] private int m_WallDensity = 3;
+    [FormerlySerializedAs("FoodDensity")][Range(0, 15)][SerializeField] private int m_FoodDensity = 2;
+    [FormerlySerializedAs("GemDensity")][Range(0, 15)][SerializeField] private int m_GemDensity = 3;
+    [FormerlySerializedAs("ChestDensity")][Range(0, 15)][SerializeField] private int m_ChestDensity = 1;
+
+    // PROPIEDADES PÚBLICAS
+    public ExitCellObject ExitCellPrefab => m_ExitCellPrefab;
+    public FoodObject[] FoodPrefabs => m_FoodPrefabs;
+    public XPGemObject[] GemPrefabs => m_GemPrefabs;
+    public ChestObject ChestPrefab => m_ChestPrefab;
+    public WallObject WallPrefab => m_WallPrefab;
+    public EnemyObject[] EnemyPrefabs => m_EnemyPrefabs;
+    public int Width { get => m_Width; set => m_Width = value; }
+    public int Height { get => m_Height; set => m_Height = value; }
+    public Tile[] GroundTiles => m_GroundTiles;
+    public Tile[] WallTiles => m_WallTiles;
+    public int EnemyDensity => m_EnemyDensity;
+    public int WallDensity => m_WallDensity;
+    public int FoodDensity => m_FoodDensity;
+    public int GemDensity => m_GemDensity;
+    public int ChestDensity => m_ChestDensity;
+
     private CellData[,] m_BoardData;
     private Tilemap m_Tilemap;
     private Grid m_Grid;
     private List<Vector2Int> m_EmptyCellsList;
 
-    // Prefabs de objetos del juego
-    [Header("Prefabs de Objetos")]
-    public FoodObject[] FoodPrefabs;
-    public XPGemObject[] GemPrefabs;
-    public ChestObject ChestPrefab;
-    public WallObject WallPrefab;
-
-    // Configuracion del tamano del tablero
-    [Header("Dimensiones del Tablero")]
-    public int Width = 8;
-    public int Height = 8;
-
-    // Tiles para el suelo y paredes
-    [Header("Tiles")]
-    public Tile[] GroundTiles;
-    public Tile[] WallTiles;
-
-    // Configuracion de enemigos
-    [Header("Enemigos")]
-    public EnemyObject[] EnemyPrefabs;
-    [Range(0, 15)]
-    public int EnemyDensity = 2;
-
-    // Densidad de generacion de objetos
-    [Header("Densidad Objetos")]
-    [Range(0, 15)]
-    public int WallDensity = 3;
-    [Range(0, 15)]
-    public int FoodDensity = 2;
-    [Range(0, 15)]
-    public int GemDensity = 3;
-    [Range(0, 15)]
-    public int ChestDensity = 1;
-
-    // Inicializa el tablero y genera todo el contenido
     public void Init()
     {
         m_Tilemap = GetComponentInChildren<Tilemap>();
@@ -62,37 +67,32 @@ public class BoardManager : MonoBehaviour
         m_EmptyCellsList = new List<Vector2Int>();
         m_BoardData = new CellData[Width, Height];
 
-        // Genera el grid base de tiles
         for (int y = 0; y < Height; ++y)
         {
             for (int x = 0; x < Width; ++x)
             {
                 Tile tile;
                 m_BoardData[x, y] = new CellData();
-
                 if (x == 0 || y == 0 || x == Width - 1 || y == Height - 1)
                 {
-                    tile = WallTiles[Random.Range(0, WallTiles.Length)];
+                    tile = m_WallTiles[Random.Range(0, m_WallTiles.Length)];
                     m_BoardData[x, y].Passable = false;
                 }
                 else
                 {
-                    tile = GroundTiles[Random.Range(0, GroundTiles.Length)];
+                    tile = m_GroundTiles[Random.Range(0, m_GroundTiles.Length)];
                     m_BoardData[x, y].Passable = true;
                     m_EmptyCellsList.Add(new Vector2Int(x, y));
                 }
-
                 m_Tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
 
-        // Coloca la salida en la esquina opuesta
         m_EmptyCellsList.Remove(new Vector2Int(1, 1));
         Vector2Int endCoord = new Vector2Int(Width - 2, Height - 2);
-        AddObject(Instantiate(ExitCellPrefab), endCoord);
+        AddObject(Instantiate(m_ExitCellPrefab), endCoord);
         m_EmptyCellsList.Remove(endCoord);
 
-        // Genera todos los objetos del nivel
         GenerateWall();
         GenerateChests();
         GenerateEnemies();
@@ -100,108 +100,85 @@ public class BoardManager : MonoBehaviour
         GenerateFood();
     }
 
-    // Genera gemas de experiencia en celdas aleatorias
     void GenerateGems()
     {
         int totalPassableCells = (Width - 2) * (Height - 2);
-        int gemCount = (totalPassableCells * GemDensity) / 27;
-
+        int gemCount = (totalPassableCells * m_GemDensity) / 27;
         if (gemCount > m_EmptyCellsList.Count) gemCount = m_EmptyCellsList.Count;
-
         for (int i = 0; i < gemCount; ++i)
         {
             int randomIndex = Random.Range(0, m_EmptyCellsList.Count);
             Vector2Int coord = m_EmptyCellsList[randomIndex];
             m_EmptyCellsList.RemoveAt(randomIndex);
-
-            XPGemObject prefabToSpawn = GemPrefabs[Random.Range(0, GemPrefabs.Length)];
+            XPGemObject prefabToSpawn = m_GemPrefabs[Random.Range(0, m_GemPrefabs.Length)];
             AddObject(Instantiate(prefabToSpawn), coord);
         }
     }
 
-    // Genera cofres con recompensas
     void GenerateChests()
     {
         int totalPassableCells = (Width - 2) * (Height - 2);
-        int chestCount = (totalPassableCells * ChestDensity) / 150;
-
+        int chestCount = (totalPassableCells * m_ChestDensity) / 150;
         if (chestCount > m_EmptyCellsList.Count) chestCount = m_EmptyCellsList.Count;
-
         for (int i = 0; i < chestCount; ++i)
         {
             int randomIndex = Random.Range(0, m_EmptyCellsList.Count);
             Vector2Int coord = m_EmptyCellsList[randomIndex];
             m_EmptyCellsList.RemoveAt(randomIndex);
-
-            ChestObject newChest = Instantiate(ChestPrefab);
+            ChestObject newChest = Instantiate(m_ChestPrefab);
             AddObject(newChest, coord);
         }
     }
 
-    // Genera comida para recuperar vida
     void GenerateFood()
     {
         int totalPassableCells = (Width - 2) * (Height - 2);
-        int foodCount = (totalPassableCells * FoodDensity) / 27;
-
-        if (foodCount > m_EmptyCellsList.Count)
-            foodCount = m_EmptyCellsList.Count;
-
+        int foodCount = (totalPassableCells * m_FoodDensity) / 27;
+        if (foodCount > m_EmptyCellsList.Count) foodCount = m_EmptyCellsList.Count;
         for (int i = 0; i < foodCount; ++i)
         {
             int randomIndex = Random.Range(0, m_EmptyCellsList.Count);
             Vector2Int coord = m_EmptyCellsList[randomIndex];
             m_EmptyCellsList.RemoveAt(randomIndex);
-
-            FoodObject prefabToSpawn = FoodPrefabs[Random.Range(0, FoodPrefabs.Length)];
+            FoodObject prefabToSpawn = m_FoodPrefabs[Random.Range(0, m_FoodPrefabs.Length)];
             AddObject(Instantiate(prefabToSpawn), coord);
         }
     }
 
-    // Genera muros destructibles
     void GenerateWall()
     {
         int totalPassableCells = (Width - 2) * (Height - 2);
-        int wallCount = (totalPassableCells * WallDensity) / 27;
-
-        if (wallCount > m_EmptyCellsList.Count)
-            wallCount = m_EmptyCellsList.Count;
-
+        int wallCount = (totalPassableCells * m_WallDensity) / 27;
+        if (wallCount > m_EmptyCellsList.Count) wallCount = m_EmptyCellsList.Count;
         for (int i = 0; i < wallCount; ++i)
         {
             int randomIndex = Random.Range(0, m_EmptyCellsList.Count);
             Vector2Int coord = m_EmptyCellsList[randomIndex];
             m_EmptyCellsList.RemoveAt(randomIndex);
-            AddObject(Instantiate(WallPrefab), coord);
+            AddObject(Instantiate(m_WallPrefab), coord);
         }
     }
 
-    // Genera enemigos en posiciones aleatorias
     void GenerateEnemies()
     {
         int totalPassableCells = (Width - 2) * (Height - 2);
-        int enemyCount = (totalPassableCells * EnemyDensity) / 20;
-
+        int enemyCount = (totalPassableCells * m_EnemyDensity) / 20;
         if (enemyCount > m_EmptyCellsList.Count) enemyCount = m_EmptyCellsList.Count;
-
         for (int i = 0; i < enemyCount; ++i)
         {
             int randomIndex = Random.Range(0, m_EmptyCellsList.Count);
             Vector2Int coord = m_EmptyCellsList[randomIndex];
             m_EmptyCellsList.RemoveAt(randomIndex);
-
-            EnemyObject prefabToSpawn = EnemyPrefabs[Random.Range(0, EnemyPrefabs.Length)];
+            EnemyObject prefabToSpawn = m_EnemyPrefabs[Random.Range(0, m_EnemyPrefabs.Length)];
             AddObject(Instantiate(prefabToSpawn), coord);
         }
     }
 
-    // Convierte coordenadas de celda a posicion mundial
     public Vector3 CellToWorld(Vector2Int cellIndex)
     {
         return m_Grid.GetCellCenterWorld((Vector3Int)cellIndex);
     }
 
-    // Obtiene los datos de una celda especifica
     public CellData GetCellData(Vector2Int cellIndex)
     {
         if (cellIndex.x < 0 || cellIndex.x >= Width || cellIndex.y < 0 || cellIndex.y >= Height)
@@ -209,7 +186,6 @@ public class BoardManager : MonoBehaviour
         return m_BoardData[cellIndex.x, cellIndex.y];
     }
 
-    // Anade un objeto a una celda del tablero
     void AddObject(CellObject obj, Vector2Int coord)
     {
         CellData data = m_BoardData[coord.x, coord.y];
@@ -218,24 +194,19 @@ public class BoardManager : MonoBehaviour
         obj.Init(coord);
     }
 
-    // Cambia el tile de una celda
     public void SetCellTile(Vector2Int cellIndex, Tile tile)
     {
         m_Tilemap.SetTile(new Vector3Int(cellIndex.x, cellIndex.y, 0), tile);
     }
 
-    // Obtiene el tile de una celda
     public Tile GetCellTile(Vector2Int cellIndex)
     {
         return m_Tilemap.GetTile<Tile>(new Vector3Int(cellIndex.x, cellIndex.y, 0));
     }
 
-    // Limpia todo el tablero
     public void Clean()
     {
-        if (m_BoardData == null)
-            return;
-
+        if (m_BoardData == null) return;
         for (int y = 0; y < m_BoardData.GetLength(1); ++y)
         {
             for (int x = 0; x < m_BoardData.GetLength(0); ++x)
--- Assets/Scripts/Data/PlayerData.cs
+++ Assets/Scripts/Data/PlayerData.cs
@@ -1,79 +1,77 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization; // NECESARIO PARA RECUPERAR DATOS
 
-// Datos del personaje del jugador
-// Almacena estadisticas, progreso y logros
 [Serializable]
 public class PlayerData
 {
-    // Estadisticas base del personaje
-    public string Name;
-    public int Strength;
-    public int Agility;
-    public int Intelligence;
-    public int Health;
+    [Header("Estadisticas Base")]
+    [FormerlySerializedAs("Name")][SerializeField] private string m_Name;
+    [FormerlySerializedAs("Strength")][SerializeField] private int m_Strength;
+    [FormerlySerializedAs("Agility")][SerializeField] private int m_Agility;
+    [FormerlySerializedAs("Intelligence")][SerializeField] private int m_Intelligence;
+    [FormerlySerializedAs("Health")][SerializeField] private int m_Health;
 
-    // Datos de partida guardada
-    public int SavedLife;
-    public int SavedSkillPoints;
-    public int SavedNight;
-    public int SavedBoardWidth;
-    public int SavedBoardHeight;
-    public int SavedEnemyHealthBonus;
-    public List<Achievement> Achievements;
+    [Header("Datos de Partida Guardada")]
+    [FormerlySerializedAs("SavedLife")][SerializeField] private int m_SavedLife;
+    [FormerlySerializedAs("SavedSkillPoints")][SerializeField] private int m_SavedSkillPoints;
+    [FormerlySerializedAs("SavedNight")][SerializeField] private int m_SavedNight;
+    [FormerlySerializedAs("SavedBoardWidth")][SerializeField] private int m_SavedBoardWidth;
+    [FormerlySerializedAs("SavedBoardHeight")][SerializeField] private int m_SavedBoardHeight;
+    [FormerlySerializedAs("SavedEnemyHealthBonus")][SerializeField] private int m_SavedEnemyHealthBonus;
+    [FormerlySerializedAs("Achievements")][SerializeField] private List<Achievement> m_Achievements;
 
-    // Records del jugador
-    public int BestLevel = 0;
-    public int BestXP = 0;
+    [Header("Records")]
+    [FormerlySerializedAs("BestLevel")][SerializeField] private int m_BestLevel = 0;
+    [FormerlySerializedAs("BestXP")][SerializeField] private int m_BestXP = 0;
 
-    // Vida inicial calculada segun salud
-    public int StartingLife
-    {
-        get { return 35 + (Health * 5); }
-    }
+    // PROPIEDADES PÚBLICAS
+    public string Name { get => m_Name; set => m_Name = value; }
+    public int Strength { get => m_Strength; set => m_Strength = value; }
+    public int Agility { get => m_Agility; set => m_Agility = value; }
+    public int Intelligence { get => m_Intelligence; set => m_Intelligence = value; }
+    public int Health { get => m_Health; set => m_Health = value; }
+    public int SavedLife { get => m_SavedLife; set => m_SavedLife = value; }
+    public int SavedSkillPoints { get => m_SavedSkillPoints; set => m_SavedSkillPoints = value; }
+    public int SavedNight { get => m_SavedNight; set => m_SavedNight = value; }
+    public int SavedBoardWidth { get => m_SavedBoardWidth; set => m_SavedBoardWidth = value; }
+    public int SavedBoardHeight { get => m_SavedBoardHeight; set => m_SavedBoardHeight = value; }
+    public int SavedEnemyHealthBonus { get => m_SavedEnemyHealthBonus; set => m_SavedEnemyHealthBonus = value; }
+    public List<Achievement> Achievements { get => m_Achievements; set => m_Achievements = value; }
+    public int BestLevel { get => m_BestLevel; set => m_BestLevel = value; }
+    public int BestXP { get => m_BestXP; set => m_BestXP = value; }
 
-    // Dano extra a enemigos por fuerza
-    public int BonusDamageToEnemies
-    {
-        get { return Strength / 5; }
-    }
+    public int StartingLife => 35 + (m_Health * 5);
+    public int BonusDamageToEnemies => m_Strength / 5;
+    public int BonusDamageToWalls => m_Agility / 5;
 
-    // Dano extra a muros por agilidad
-    public int BonusDamageToWalls
-    {
-        get { return Agility / 5; }
-    }
-
-    // Probabilidad de esquivar ataques por inteligencia
     public float DodgeChance
     {
         get
         {
-            float chance = (Intelligence / 5) * 0.05f;
+            float chance = (m_Intelligence / 5) * 0.05f;
             return Mathf.Min(chance, 0.9f);
         }
     }
 
-    // Constructor completo
     public PlayerData(string name, int strength, int agility, int intelligence, int health)
     {
-        Name = name;
-        Strength = strength;
-        Agility = agility;
-        Intelligence = intelligence;
-        Health = health;
-        SavedLife = -1;
-        SavedSkillPoints = 0;
-        SavedNight = 1;
-        SavedBoardWidth = 8;
-        SavedBoardHeight = 8;
-        SavedEnemyHealthBonus = 0;
-        Achievements = null;
-        BestLevel = 0;
-        BestXP = 0;
+        m_Name = name;
+        m_Strength = strength;
+        m_Agility = agility;
+        m_Intelligence = intelligence;
+        m_Health = health;
+        m_SavedLife = -1;
+        m_SavedSkillPoints = 0;
+        m_SavedNight = 1;
+        m_SavedBoardWidth = 8;
+        m_SavedBoardHeight = 8;
+        m_SavedEnemyHealthBonus = 0;
+        m_Achievements = null;
+        m_BestLevel = 0;
+        m_BestXP = 0;
     }
 
-    // Constructor vacio para deserializacion
     public PlayerData() { }
 }
